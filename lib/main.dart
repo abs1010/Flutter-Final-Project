f4bacaa2215472e1bf2b10c00ea2f1737bdde8c9
Alan Silva <alan.bezerra@xpi.com.br>
2022-07-02 19:53:47 -0300

    firebase Core

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:projeto_cadastro_final/UI/movies_screen.dart';
 import 'package:projeto_cadastro_final/main/pages/movies_screen_factory.dart';
+import 'firebase_options.dart';
 //import 'package:flutter_native_splash/flutter_native_splash.dart';
-//import 'package:firebase_core/firebase_core.dart';
-//import 'firebase_options.dart';
 
 void main() async {
-  //WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   //FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
-  // await Firebase.initializeApp(
-  //   options: DefaultFirebaseOptions.currentPlatform,
-  // );
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(const MyApp());
 }
